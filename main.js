fba53f9b34d2945bdb398713d487961c241db6d2
Joseph <codyranch3810@gmail.com>
2025-08-24 03:01:47 +0800

    Vr1

---
+++ main.js
@@ -0,0 +1,36 @@
+import "./assets/scss/all.scss";
+
+var bar = new ProgressBar.Circle("#circle-container", {
+  color: "#ffffff", // 👈 改成白色
+  strokeWidth: 8,
+  trailColor: "transparent", // 沒有灰色外框
+  trailWidth: 8,
+  text: {
+    value: "0%",
+    style: {
+      fontSize: "16px",
+      fontWeight: "bold",
+      color: "#fff",
+      position: "absolute",
+      left: "50%",
+      top: "50%",
+      transform: "translate(-50%, -50%)",
+    },
+  },
+  step: function (state, circle) {
+    let value = Math.round(circle.value() * 100);
+    circle.setText(value + "%");
+
+    if (value === 100) {
+      document.getElementById("status-text").innerText = "分析完成";
+      setTimeout(() => {
+        window.location.href = "record.html";
+      }, 1000);
+    }
+  },
+});
+
+// 初始完全空
+bar.set(0);
+// 動畫跑滿
+bar.animate(1.0, { duration: 4000 });
